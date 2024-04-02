1f5003caf75758fc1afd6cfff640a4acc8c77ed0
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-04-02 14:59:40 -0300

    Initial commit

---
+++ src/main/java/io/github/marcelomartinsdev/cleanarchapi/CleanarchApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.marcelomartinsdev.cleanarchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/marcelomartinsdev/cleanarchapi/CleanarchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marcelomartinsdev.cleanarchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
